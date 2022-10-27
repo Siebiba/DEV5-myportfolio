4dc19b3049f9f45044d8e53cd33e8bac50ca5d3c
Siebe <r0834360@student.thomasmore.be>
2022-10-27 17:48:13 +0200

    feat: check for winner when clicking a card

--- lab2-bingo/bingo/js/card.js
+++ lab2-bingo/bingo/js/card.js
@@ -21,14 +21,22 @@ export default class Card {
     console.log("Rendering card...");
 
     // 🔥🔥🔥 TODO3: build the HTML element and append it to the DOM
-    // let card = document.createElement("div");
-    // card.dataset.number = counter + 1;
+    let card = document.createElement("div");
+    
+    card.dataset.number = counter + 1;
+
     // don't forget to append the child to to DOM
+    document.querySelector(".bingo__cards").appendChild(card);
+
 
     // 🔥🔥🔥 TODO4: when we click an item, we want to check for winners and we want to save the selection to storage
     card.addEventListener("click", (e) => {
-      // this.markDone(e.target);
+    this.markDone(e.target);
+    console.log("click");
       // call checkWinner() on the Bingo class
+      Bingo.checkWinner();
+      // call save() on the Bingo class
+      Bingo.save();
       // try to call the save() method on the Bingo class
     });
   }

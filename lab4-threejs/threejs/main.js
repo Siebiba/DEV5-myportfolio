e2a8723ac610cc8dafa5af1ea14d4434eea3d1bd
Siebe <r0834360@student.thomasmore.be>
2022-12-06 17:24:28 +0100

    feat: rotating 3D cube

--- lab4-threejs/threejs/main.js
+++ lab4-threejs/threejs/main.js
@@ -1,49 +1,64 @@
-import "./style.css";
-import * as THREE from "three";
-import { OrbitControls} from "three/examples/jsm/controls/OrbitControls"
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
+
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 
 
-
+//Scene
 const scene = new THREE.Scene();
+//Camera
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 5; // <- New code
+//Renderer
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setClearColor("#233143");
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+// Make Canvas Responsive
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+})
 
+
+// Create Box
+const boxGeometry = new THREE.BoxGeometry(2, 2, 2);
+const boxMaterial = new THREE.MeshLambertMaterial({color: 0xFFFFFF});
+const boxMesh = new THREE.Mesh(boxGeometry, 
+boxMaterial);
+boxMesh.rotation.set(40, 0, 40);
+scene.add(boxMesh);
+
+//Lights
+const light = new THREE.PointLight(0xFFFFFF, 1, 100);
+light.position.set(5, 5, 5);
+scene.add(light);
+
+//orbit Controls for Camera 
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.screenSpacePanning = false;
+controls.minDistance = 1;
+controls.maxDistance = 5;
 
-const gridHelper = new THREE.GridHelper(200 , 50);
-scene.add( gridHelper);
-const light = new THREE.AmbientLight( 0x404040 ); // soft white light
-scene.add( light );
-const directionalLight = new THREE.DirectionalLight( 0x00ffff, 0.5 );
-scene.add( directionalLight );
 
-const directionalLightHelper = new THREE.DirectionalLightHelper(
-  directionalLight, 
-  2
-);
+camera.position.set( 0, 20, 100 );
+controls.update();
 
-scene.add(directionalLightHelper);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
-//scene renderer
+// Rendering Function
+const rendering = function() {
+    requestAnimationFrame(rendering);
 
-function animate() {
-  requestAnimationFrame( animate );
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  renderer.render( scene, camera );
-};
-
-animate();
+    // Update trackball controls
+    controls.update();
+    // Constantly rotate box
+    scene.rotation.z -= 0.005;
+    scene.rotation.x -= 0.01;
+    renderer.render(scene, camera);
+}
+rendering();

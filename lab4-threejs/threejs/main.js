222b1b8428aedef099fb2faf87a5804b3008a31e
Siebe <r0834360@student.thomasmore.be>
2022-12-06 22:34:15 +0100

    feat: add controls

--- lab4-threejs/threejs/main.js
+++ lab4-threejs/threejs/main.js
@@ -1,19 +1,32 @@
 
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-
+//import floor from src
+import Floor from './src/floor.js';
+import House from './src/house.js';
 
 
 //Scene
 const scene = new THREE.Scene();
 //Camera
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 5; // <- New code
+
 //Renderer
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setClearColor("#233143");
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+//ambient light
+const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5);
+scene.add(ambientLight);
+
+
+//Lights
+const light = new THREE.PointLight(0xFFFFFF, 1, 100);
+light.position.set(5, 5, 5);
+scene.add(light);
+
 
 // Make Canvas Responsive
 window.addEventListener('resize', () => {
@@ -22,43 +35,40 @@ window.addEventListener('resize', () => {
     camera.updateProjectionMatrix();
 })
 
+const floor = new Floor();
+scene.add(floor.group);
 
-// Create Box
-const boxGeometry = new THREE.BoxGeometry(2, 2, 2);
-const boxMaterial = new THREE.MeshLambertMaterial({color: 0xFFFFFF});
-const boxMesh = new THREE.Mesh(boxGeometry, 
-boxMaterial);
-boxMesh.rotation.set(40, 0, 40);
-scene.add(boxMesh);
+const house = new House();
+scene.add(house.group);
 
-//Lights
-const light = new THREE.PointLight(0xFFFFFF, 1, 100);
-light.position.set(5, 5, 5);
-scene.add(light);
 
-//orbit Controls for Camera 
+
+
+
+
+
+
+//add controls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.05;
 controls.screenSpacePanning = false;
-controls.minDistance = 1;
-controls.maxDistance = 5;
-
+controls.minDistance = 5;
+controls.maxDistance = 10;
 
 camera.position.set( 0, 20, 100 );
 controls.update();
-
 
 
 // Rendering Function
 const rendering = function() {
     requestAnimationFrame(rendering);
 
-    // Update trackball controls
-    controls.update();
+    // Update controls
+    
+    
     // Constantly rotate box
-    scene.rotation.z -= 0.005;
-    scene.rotation.x -= 0.01;
+    
     renderer.render(scene, camera);
 }
 rendering();
